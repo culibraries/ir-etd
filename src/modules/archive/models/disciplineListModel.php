1058c2f7f05e3cdd7b4f815cccbf3cadc9c5b434
Fred Schumacher <fred.schumacher@Colorado.EDU>
2017-04-24 16:01:48 -0600

    Added disciplines list class

---
+++ src/modules/archive/models/disciplineListModel.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * DisciplineListModel
+ *
+ * @author    Fred Schumacher <fred.schumacher@colorado.edu>
+ * @version   1.0.0 Initial release
+ * @copyright Copyright (c) 2017 University Libraries, CU Boulder
+ */
+
+ /**
+ * Represents a list of valid disciplines
+ */
+require_once($_SERVER['DOCUMENT_ROOT'] . '/etd/resources/config.php');
+
+class DisciplineListModel
+{
+
+	private $conn; // connection to the MySQL database
+	private $db;   // database instance itself
+
+	private $disciplines; // list of valid disciplines
+
+	/**
+	 * constructor
+	 *
+	 * Establishes a database connection using specified datasource. Database
+	 * connection remains open throughout the life of the object.
+	 */
+	public function __construct()
+	{
+		global $config;
+		require(MODULES_PATH . '/archive/models/connectionModel.php');
+		$this->conn = new ConnectionModel($config['db']);
+		$this->db = $this->conn->open();
+
+		$this->getDisciplines();
+	}
+
+  /**
+	 * destructor
+	 *
+	 * Closes database connection when the object is destroyed.
+	 */
+	public function __destruct()
+	{
+		$this->conn->close();
+	}
+
+	/**
+	 * getDisciplines
+	 *
+	 * Gets list of disciplines for the reference table.
+	 */
+	 private function getDisciplines()
+	 {
+		 $sql = "SELECT * FROM discipline_ref";
+
+		 $result = $this->db->query($sql);
+		 $this->disciplines = array();
+		 	while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+ 				$this->disciplines[] = $row["discipline_name"];
+ 			}
+
+	 }
+
+	 /**
+ 	 * isValidDiscipline
+ 	 *
+ 	 * Checks the validity of submitted discipline, i.e., is it in
+	 * the database. Commas and other punctuation are stripped from
+	 * the search term before the check is made.
+	 *
+	 * returns TRUE or FALSE
+ 	 */
+	 public function isValidDiscipline($discipline)
+	 {
+		 $discipline = strtolower(str_replace(array(",","'"), "", $discipline));
+		 return in_array($discipline, $this->disciplines);
+	 }
+
+}
+?>
